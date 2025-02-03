ce33740422f91de2f5e15f25ccfeb75a16bf5af7
Alejandro <alejandropino88@gmail.com>
2025-02-03 21:42:46 +0100

    portal effect improvements

--- scripts/challenge_portal.gd
+++ scripts/challenge_portal.gd
@@ -3,11 +3,20 @@ extends Node2D
 class_name ChallengePortal
 
 @onready var area: Area2D = $Area2D
+@onready var sprite: AnimatedSprite2D = $PortalInside
+
 @export var enter: bool = true
 @export var active: bool = true
+@export var portal_color: Color = Color(1, 1, 1, 1)
 
 func _ready() -> void:
 	area.body_entered.connect(detect_player)
+
+func _process(delta: float) -> void:
+	if active:
+		sprite.modulate = lerp(sprite.modulate, Color(portal_color.r, portal_color.g, portal_color.b, 1), 0.1 * delta * 60.0)
+	else:
+		sprite.modulate = lerp(sprite.modulate, Color(portal_color.r, portal_color.g, portal_color.b, 0), 0.1 * delta * 60.0)
 
 func detect_player(body: Node) -> void:
 	if !active:
