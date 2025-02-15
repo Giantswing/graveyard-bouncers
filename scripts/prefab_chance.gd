8c4213d46f43d196b11948ae07077d88dc15ed5f
Alejandro <alejandropino88@gmail.com>
2025-02-15 15:31:06 +0100

    stash

--- scripts/prefab_chance.gd
+++ scripts/prefab_chance.gd
@@ -11,6 +11,20 @@ enum SPAWN_POS_OPTIONS {
 
 @export var name: String
 @export var prefab: PackedScene
+
 @export var chance: float = 1.0
+@export var chance_decay: float = 1.0
 @export var free_space: float = 30.0
 @export var spawn_type: SPAWN_POS_OPTIONS = SPAWN_POS_OPTIONS.GROUND
+
+var original_chance: float
+
+func init() -> void:
+	original_chance = chance
+
+func select() -> PrefabChance:
+	chance *= chance_decay
+	return self
+
+func reset() -> void:
+	chance = original_chance
